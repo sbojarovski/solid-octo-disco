0d00d7b9783baf5c93310b101ca3693b8ac41b12
Stefan Bojarovski <bojarovski@gmail.com>
2020-07-31 15:42:58 +0200

    prepare database access and environment

--- monitor/src/settings.py
+++ monitor/src/settings.py
@@ -3,3 +3,9 @@ import os
 KAFKA_HOST = os.environ.get('KAFKA_CONTAINER_NAME', 'kafka')
 KAFKA_PORT = os.environ.get('KAFKA_PORT', 9092)
 KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC', 'website-health')
+
+DB_USER = os.environ.get('DB_USER', 'monitor_db_user')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'monitor_db_password')
+DB_HOST = os.environ.get('DB_HOST', 'db')
+DB_PORT = os.environ.get('DB_PORT', 5432)
+DB_NAME = os.environ.get('DB_NAME', 'monitor_db')
